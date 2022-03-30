3c1c739aa32ff1e83f4b97d75960a617924ed892
Sagar Sharma <53833815+sagar0028@users.noreply.github.com>
2022-03-30 23:08:21 +0530

    using another method

--- Character Arrays and 2D Arrays/CompressString.cpp
+++ Character Arrays and 2D Arrays/CompressString.cpp
@@ -44,3 +44,41 @@ void stringCompression(char input[]){
         nextIndex++;
     }input[nextIndex] = '\0';
 }
+/*-----------------------------------------ANOTHER SOLUTION-----------------------------------------*/
+
+string getCompressedString(string &input) 
+{
+    int n=input.length();
+    if(n==0)
+    {
+        return "";
+        
+    }
+    int s=0;
+    int e=0;
+    
+    string str="";
+    
+    while(s<n)
+    {
+        while((s<n)&&input[s]==input[e])
+        {
+            e++;
+        }
+        
+        int totalchar=e-s;
+        
+        if(totalchar!=1)
+        {
+            str=str+input[s];
+            str=str+(char)(totalchar+'0');
+        }
+        else
+        {
+            str=str+input[s];
+        }
+        s=e;
+    }
+    return str;
+    
+}
